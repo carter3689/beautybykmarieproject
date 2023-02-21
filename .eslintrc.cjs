e2b6beeb3759c723ac4d57dcecc475c31da03b4d
Joel Carter <carter3689@gmail.com>
2023-02-21 12:12:55 -0600

    updating eslintrc file with no linting for deploy faster

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,13 @@ module.exports = {
         prefer: "type-imports",
         fixStyle: "inline-type-imports",
       },
+      {
+        "extends": "next",
+        "rules": {
+          "react/no-unescaped-entities": "off",
+          "@next/next/no-page-custom-font": "off"
+        }
+      }
     ],
   },
 };
